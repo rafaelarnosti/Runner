12a09ad2314f7f0f8029fe990bdb9313264fe6b1
Rafael Arnosti <rlm.arnosti@gmail.com>
2018-03-12 20:36:34 -0300

    Initial commit

---
+++ Assets/scripts/PersonagemScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonagemScript : MonoBehaviour {
+
+
+	Vector3 novaPosicao;
+
+	void Start () {
+		//posição inicial personagem
+		novaPosicao = transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//touch ou clique do mouse
+
+		if (Input.GetButton ("Fire1")) {
+
+			RaycastHit hit;
+
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			if (Physics.Raycast (ray, out hit)) {
+
+				novaPosicao = hit.point;
+				transform.position = novaPosicao;
+			}
+
+
+		}
+
+		
+	}
+}
