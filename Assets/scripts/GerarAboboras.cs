5a3e65c6841c01eac7ed419e183369ded0f15e41
Rafael Arnosti <rlm.arnosti@gmail.com>
2018-03-12 22:44:07 -0300

    commit

---
+++ Assets/scripts/GerarAboboras.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerarAboboras : MonoBehaviour {
+
+	public float limiteEsquerdo, limiteDireito, limiteFrontal, limiteTraseiro;
+	public float frequencia;
+	public GameObject aboboraPrefab;
+
+
+	IEnumerator Start(){
+		yield return new WaitForSeconds (Random.Range (0.5f, frequencia));
+		Vector3 posicao;
+		posicao.x = Random.Range (limiteEsquerdo, limiteDireito);
+		posicao.y = transform.position.y;
+	    posicao.z = Random.Range (limiteFrontal, limiteTraseiro);
+		Instantiate (aboboraPrefab, posicao, transform.rotation);
+		StartCoroutine (Start ());
+	}
+
+}
--- Assets/scripts/PersonagemScript.cs
+++ Assets/scripts/PersonagemScript.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class PersonagemScript : MonoBehaviour {
 
-
+	public GameObject personagem;
+	public float velocidade;
 	Vector3 novaPosicao;
+	Animation ani;
+
+	void OnCollisionEnter(Collision c){
+		if (c.gameObject.tag == "item") {
+			Destroy (c.gameObject);
+		}
+	}
 
 	void Start () {
 		//posição inicial personagem
 		novaPosicao = transform.position;
+		ani = personagem.GetComponent<Animation> ();
+		//define a animação inicial do personagem
+		ani.CrossFade ("idle");
 		
 	}
 	
@@ -23,15 +34,20 @@ public class PersonagemScript : MonoBehaviour {
 
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 
-			if (Physics.Raycast (ray, out hit)) {
-
+			if (Physics.Raycast (ray, out hit) ) {
 				novaPosicao = hit.point;
-				transform.position = novaPosicao;
+				//transform.position = novaPosicao;
+				transform.position = Vector3.MoveTowards (transform.position, novaPosicao, velocidade * Time.deltaTime);
+				ani.CrossFade ("run");			
+
+				transform.LookAt (hit.point);
+
+			} else {
+				ani.CrossFade ("idle");
 			}
 
 
 		}
-
 		
 	}
 }
